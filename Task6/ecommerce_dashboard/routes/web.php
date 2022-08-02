a2ad89fdee8c233ef8f36a7622b38ffe705a3b1a
Omar Tareq <es-omar.tarek1217@alexu.edu.eg>
2022-08-02 09:03:08 +0200

    task6

---
+++ Task6/ecommerce_dashboard/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('dashboard/products', [ProductController::class, 'index'])->name('dash.products');
+Route::get('dashboard/products/create', [ProductController::class, 'create'])->name('dash.products.create');
+Route::get('dashboard/products/edit/{$id}', [ProductController::class, 'eidt'])->name('dash.products.edit');
+Route::post('dashboard/products/store', [ProductController::class, 'store'])->name('dash.products.store');
